b1f2ab3e532cc535d1ddaab734d76d4e730792d9
Franco Hidalgo <francohidalgo2016@gmail.com>
2026-07-09 22:16:40 -0300

    mejora de fixture

--- app/fixture.py
+++ app/fixture.py
@@ -16,79 +16,79 @@ FIXTURE = [
     {
         "ronda": 2,
         "cancha": 1,
-        "equipo1": [8, 5],
-        "equipo2": [4, 6]
+        "equipo1": [6, 7],
+        "equipo2": [3, 2]
     },
     {
         "ronda": 2,
         "cancha": 2,
-        "equipo1": [3, 7],
-        "equipo2": [2, 1]
+        "equipo1": [1, 5],
+        "equipo2": [8, 4]
     },
 
     {
         "ronda": 3,
         "cancha": 1,
-        "equipo1": [2, 8],
-        "equipo2": [1, 3]
+        "equipo1": [6, 2],
+        "equipo2": [1, 4]
     },
     {
         "ronda": 3,
         "cancha": 2,
-        "equipo1": [7, 4],
-        "equipo2": [6, 5]
+        "equipo1": [3, 7],
+        "equipo2": [5, 8]
     },
 
     {
         "ronda": 4,
         "cancha": 1,
-        "equipo1": [8, 6],
+        "equipo1": [4, 2],
         "equipo2": [5, 7]
     },
     {
         "ronda": 4,
         "cancha": 2,
-        "equipo1": [4, 1],
-        "equipo2": [3, 2]
-    },
-
-    {
-        "ronda": 5,
-        "cancha": 1,
         "equipo1": [3, 8],
-        "equipo2": [2, 4]
+        "equipo2": [1, 6]
+    },
+
+    {
+        "ronda": 5,
+        "cancha": 1,
+        "equipo1": [3, 1],
+        "equipo2": [7, 4]
     },
     {
         "ronda": 5,
         "cancha": 2,
-        "equipo1": [1, 5],
-        "equipo2": [7, 6]
+        "equipo1": [6, 8],
+        "equipo2": [2, 5]
     },
 
     {
         "ronda": 6,
         "cancha": 1,
+        "equipo1": [3, 4],
+        "equipo2": [8, 2]
+    },
+    {
+        "ronda": 6,
+        "cancha": 2,
+        "equipo1": [1, 7],
+        "equipo2": [6, 5]
+    },
+
+    {
+        "ronda": 7,
+        "cancha": 1,
+        "equipo1": [3, 5],
+        "equipo2": [1, 2]
+    },
+    {
+        "ronda": 7,
+        "cancha": 2,
         "equipo1": [8, 7],
-        "equipo2": [6, 1]
-    },
-    {
-        "ronda": 6,
-        "cancha": 2,
-        "equipo1": [5, 2],
-        "equipo2": [4, 3]
-    },
-
-    {
-        "ronda": 7,
-        "cancha": 1,
-        "equipo1": [4, 8],
-        "equipo2": [5, 3]
-    },
-    {
-        "ronda": 7,
-        "cancha": 2,
-        "equipo1": [2, 6],
-        "equipo2": [1, 7]
+        "equipo2": [6, 4]
     }
 
 ]
